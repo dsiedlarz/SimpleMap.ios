000c5337b32d4c47795fa78bd04bbc6f51cf0c80
dsiedlarz <dsiedlarz@gmail.com>
2017-10-29 22:28:55 +0100

    Initial Commit

---
+++ SimpleMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleMap
+//
+//  Created by Dawid Siedlarz private on 29.10.2017.
+//  Copyright © 2017 Dawid Siedlarz private. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
